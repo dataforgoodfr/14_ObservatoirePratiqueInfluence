74a1e4f399ed85f6a06f98c980432514b421d733
Sébastien Arod <sebastien.arod@gmail.com>
2026-03-29 09:49:36 +0200

    fix: fix mypy and ruff errors

--- data-extractors/src/data_extractors/tiktok/tiktok_sb.py
+++ data-extractors/src/data_extractors/tiktok/tiktok_sb.py
@@ -1,12 +1,18 @@
 import datetime
 
-class Video():
-    def __init__(self, post_html):
-        self.url = post_html.get_attribute('href')
-        self.id = self.url.split('/')[-1]
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class Video:
+    def __init__(self, post_html: WebElement) -> None:
+        href = post_html.get_attribute("href")
+        assert href is not None
+        self.url: str = href
+        self.id: str = self.url.split("/")[-1]
         self._get_video_date()
 
-    def _get_video_date(self):
-        self.date = datetime.datetime.fromtimestamp(int(self.id) >> 32,
-                                                    datetime.UTC)
-
+    def _get_video_date(self) -> None:
+        self.date: datetime.datetime = datetime.datetime.fromtimestamp(
+            int(self.id) >> 32,
+            datetime.UTC,
+        )
