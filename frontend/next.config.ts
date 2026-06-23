4f0c87daf4667f303f216ca162f65857e68de84a
Sébastien Arod <sebastien.arod@gmail.com>
2026-06-23 08:50:51 +0200

    deploy: setup deploy gh pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  // Note:
+  // GH pages deployment generally require to configure a basePath.
+  // However this is not needed here as we have mapped a subdomain in gh config (observatoire.payetoninfluence.org)
 };
 
 export default nextConfig;
